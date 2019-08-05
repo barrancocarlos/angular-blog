871f99582faeda69a494950c57be7d2b3dc76545
Carlos Barranco <barrancocarlos@hotmail.com>
2019-08-05 20:42:49 -0300

    first

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,33 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+import { MyPostsComponent } from './components/my-posts/my-posts.component';
+import { SinglePostComponent } from './components/single-post/single-post.component';
+import { AddPostComponent } from './components/add-post/add-post.component';
+import { EditPostComponent } from './components/edit-post/edit-post.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: HomeComponent
+  },
+  {
+    path: 'myposts',
+    component: MyPostsComponent
+  },
+  {
+    path: 'single-post',
+    component: SinglePostComponent
+  },
+  {
+    path: 'add-post',
+    component: AddPostComponent
+  },
+  {
+    path: 'edit-post',
+    component: EditPostComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './components/home/home.component';
+import { MyPostsComponent } from './components/my-posts/my-posts.component';
+import { SinglePostComponent } from './components/single-post/single-post.component';
+import { AddPostComponent } from './components/add-post/add-post.component';
+import { EditPostComponent } from './components/edit-post/edit-post.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    MyPostsComponent,
+    SinglePostComponent,
+    AddPostComponent,
+    EditPostComponent
   ],
   imports: [
     BrowserModule,
