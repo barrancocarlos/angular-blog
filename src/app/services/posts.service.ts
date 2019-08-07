552ff87401f78053c5d182e9ac8470a387a7c3cd
Carlos Barranco <barrancocarlos@hotmail.com>
2019-08-07 18:35:23 -0300

    get all

---
+++ src/app/services/posts.service.ts
@@ -0,0 +1,51 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+import { Observable } from 'rxjs';
+
+import { GlobalService } from '../services/global.service';
+
+export interface PostResponse {
+  id: string;
+  title: string;
+  content: string;
+  created_at: Date;
+  updated_at: Date;
+  author: any; // change when author response
+}
+
+@Injectable({
+  providedIn: 'root'
+})
+export class PostsService {
+
+  uri = 'api/posts/';
+
+
+  constructor(private http: HttpClient, private global: GlobalService) { }
+
+  getEntities(): Observable<PostResponse[]> {
+    return this.http
+      .get<PostResponse[]>(this.global.BASE_URI + this.uri, { responseType: 'json' });
+  }
+
+  getEntity(id) {
+    return this.http
+      .get<PostResponse>(this.global.BASE_URI + this.uri + id, { responseType: 'json' });
+  }
+
+  postEntity(data) {
+    return this.http
+      .post<PostResponse>(this.global.BASE_URI + this.uri, data);
+  }
+
+  putEntity(id, data) {
+    return this.http
+      .put(this.global.BASE_URI + this.uri + id + '/', data);
+  }
+  deleteEntity(id) {
+    return this.http
+      .delete(this.global.BASE_URI + this.uri + id);
+  }
+
+}
