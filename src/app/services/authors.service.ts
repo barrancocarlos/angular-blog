6f560c256feaf35e74fef0bac718dc947ab85cc9
Carlos Barranco <barrancocarlos@hotmail.com>
2019-08-22 20:57:52 -0300

    author service

--- src/app/services/authors.service.ts
+++ src/app/services/authors.service.ts
@@ -1,9 +1,50 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+import { Observable } from 'rxjs';
+
+import { GlobalService } from '../services/global.service';
+
+export interface AuthorResponse {
+  id: string;
+  email: string;
+  password: string;
+  name: string;
+  last_name: string;
+  user: any;
+}
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthorsService {
 
-  constructor() { }
+  uri = 'api/authors/';
+
+  constructor(private http: HttpClient, private global: GlobalService) { }
+
+  getEntities(): Observable<AuthorResponse[]> {
+    return this.http
+      .get<AuthorResponse[]>(this.global.BASE_URI + this.uri, { responseType: 'json' });
+  }
+
+  getEntity(id) {
+    return this.http
+      .get<AuthorResponse>(this.global.BASE_URI + this.uri + id, { responseType: 'json' });
+  }
+
+  postEntity(data) {
+    return this.http
+      .post<AuthorResponse>(this.global.BASE_URI + this.uri, data);
+  }
+
+  putEntity(id, data) {
+    return this.http
+      .put(this.global.BASE_URI + this.uri + id + '/', data);
+  }
+  deleteEntity(id) {
+    return this.http
+      .delete(this.global.BASE_URI + this.uri + id);
+  }
+
 }
